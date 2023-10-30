8b3d2613dad7f6e230c73decd09561da2057cd07
Jakob Willforss <jakob.willforss@hotmail.com>
2023-10-30 18:03:41 +0100

    Cleanup of commands. Add qualhist. Add add prefix.

--- README.md
+++ README.md
@@ -2,4 +2,14 @@ Scripts used to slice and dice VCF-files during my daily work.
 
 Some commands assumes that you have `bgzip` and `tabix` installed in your `PATH`.
 
-Might make sense to wrap this into a cohesive tool at some point. We will see.
+Main entry point is "vtk".
+
+I.e.:
+
+```
+vtk peek my.vcf.gz
+```
+
+Wishlist:
+
+* Histogram of 
--- vcf_prepare_rtg.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import argparse
-
-parser = argparse.ArgumentParser(description="View histogram of qual scores")
-parser.add_argument("-i", "--input", help="Input VCF file", required=True)
-parser.add_argument("-o", "--output", help="Output histogram", required=True)
-parser.add_argument("--version", action="version", version="v0.1")
-args = parser.parse_args()
