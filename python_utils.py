abedc923c04496482511a9e908de84eb4235cdd9
Jakob Willforss <jakob.willforss@hotmail.com>
2024-01-11 13:46:50 +0100

    Quick diff VCFs

--- python_utils.py
+++ python_utils.py
@@ -1,5 +1,5 @@
 from typing import Callable
-from pysam import VariantFile
+from pysam import VariantFile, VariantRecord
 
 
 def filter_info(
@@ -38,3 +38,33 @@ def filter_info(
                         print(record, end="")
                     elif type == "less" and info_val_float <= comp_val_float:
                         print(record, end="")
+
+
+def snv_diff(vcf1: str, vcf2: str, print_recs: bool):
+    vcf1_recs = make_recs_dict(vcf1)
+    vcf2_recs = make_recs_dict(vcf2)
+
+    vcf1_keys = set(vcf1_recs.keys())
+    vcf2_keys = set(vcf2_recs.keys())
+
+    vcf1_only = vcf1_keys.difference(vcf2_keys)
+    vcf2_only = vcf2_keys.difference(vcf1_keys)
+
+    if not print_recs:
+        print(f"{len(vcf1_only)} only in VCF1, {len(vcf2_only)} only in VCF2")
+    else:
+        for key in vcf1_only:
+            rec = vcf1_recs[key]
+            print(f"vcf1\t{rec}")
+        for key in vcf2_only:
+            rec = vcf2_recs[key]
+            print(f"vcf2\t{rec}")
+
+
+def make_recs_dict(vcf_path: str) -> dict[str, VariantRecord]:
+    fh = VariantFile(vcf_path)
+    variant_recs = dict()
+    for record in fh:
+        key = f"{record.chrom}:{record.pos}:{'/'.join(record.alts)}"
+        variant_recs[key] = record
+    return variant_recs
